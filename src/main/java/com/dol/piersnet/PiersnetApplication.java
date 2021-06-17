92c8dffed142c99e41fa7aae65742ac528852ee5
piersnet <lcd713286@gmail.com>
2021-06-17 16:16:47 +0900

    init

---
+++ src/main/java/com/dol/piersnet/PiersnetApplication.java
@@ -0,0 +1,13 @@
+package com.dol.piersnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiersnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiersnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dol/piersnet/PiersnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dol.piersnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiersnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
